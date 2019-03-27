f048f5feaa5dce4167583175a39e505987aa98d6
Maxim <maxim3631@gmail.com>
2019-03-27 18:31:13 +0100

    modify algorithm of logistic regression

--- node_code.py
+++ node_code.py
@@ -1,67 +1,58 @@
-def node_log_regr(data_file, coefficients_file, site_number):
+import pandas as pd
+import numpy as np
+import os
 
-    import pandas as pd
-    import numpy as np
+
+def node_log_regr(coefficients):
 
 
-    #data.DC<-read.table(file=DC2.data.file, sep=",",header=T)
-    #Strip out header row
-    #data.DC<-data.DC[,-1]
+    #read database file
     node_data = pd.read_csv(os.environ["DATABASE_URI"], sep = ",")
 
 
     #Calculate number of subjects available in the current study
     #(by enumerating length of ID column)
-    #nsubs<-length(data.DC$id)
     rows_number = len(node_data)
 
     #Define design matrix (matrix of covariates) to contain BMI, SNP and the
     #interaction covariate and add a column of 1s at the start for the regression constant
-    #X.mat<-cbind(rep(1,nsubs),data.DC$bmi,data.DC$bmi456,data.DC$snp)
     design_matrix = pd.concat([pd.Series(np.repeat([1], [rows_number], axis = 0)),
-                              node_data["bmi"], node_data["bmi456"], node_data["snp"]], axis = 1).values
+                              node_data["bmi"], node_data["bmi456"],
+                               node_data["snp"]], axis = 1).values
 
 
-    #Load the current value of the beta vector (vector of regression coefficients) from its
+    #Load the current value of the beta vector
+    # (vector of regression coefficients) from its
     #location on the AC computer (stored during activation of block 2 of R code)
-    #load(file=paste(AC.Directory,"beta.vect.next.RData",sep=""))
+    beta = coefficients
 
-    beta_next = pd.read_csv(coefficients_file, header=None, sep ="\t").values
 
-    # Use this current value of the beta vector to calculate elements from the current study
-    #beta.vect<-beta.vect.next
-    beta = beta_next
-    # Calculate linear predictors from observed covariate values and elements of
+    # Calculate linear predictors from observed covariate values
+    # and elements of
     # current beta vector
-    #lp.current<-beta.vect[1]+beta.vect[2]*X.mat[,2]+beta.vect[3]*X.mat[,3]+ beta.vect[4]*X.mat[,4]
     linear_predictor = np.dot(design_matrix, beta)
 
 
     # Apply inverse logistic transformation
-    #mu.current<-exp(lp.current)/(1+exp(lp.current))
     predictor_exp = np.exp(linear_predictor)
     mu = predictor_exp/(1 + predictor_exp)
 
-    # Derive variance function and diagonal elements for weight matrix (using squared
+    # Derive variance function and diagonal elements for
+    # weight matrix (using squared
     # first differential of link function)
-    #var.i<-(mu.current*(1-mu.current))
-    #g2.i<-(1/(mu.current*(1-mu.current)))^2
-    #W.mat<-diag(1/(var.i*g2.i))
     variance = mu * (1 - mu)
     link_function_differential = 1 / (variance)
     weight_matrix = np.diagflat(variance)
 
     #Calculate information matrix
-    #info.matrix<-t(X.mat)%*%W.mat%*%X.mat
-    info_matrix = np.dot(np.dot(design_matrix.transpose(), weight_matrix), design_matrix)
+    info_matrix = np.dot(np.dot(design_matrix.transpose(),
+                                weight_matrix), design_matrix)
 
     #Derive u terms for score vector
-    #u.i<- (data.DC$CC-mu.current)* (1/(mu.current*(1-mu.current)))
     outcome = np.matrix(node_data["CC"].values).T
     u_terms = np.multiply((outcome - mu), link_function_differential)
 
     #Calculate score vector
-    #score.vect<-t(X.mat)%*%W.mat%*%u.i
     score_vect = np.dot(np.dot(design_matrix.T, weight_matrix), u_terms)
 
     #Calculate log likelihood and deviance contribution for current study
@@ -69,15 +60,18 @@ def node_log_regr(data_file, coefficients_file, site_number):
     # model, because that will cancel out in calculating the change in deviance from one
     # iteration to the next (Dev.total – Dev.old [see below]) because the element relating
     # to the saturated model will be the same at every iteration).
-    #log.L<-sum(data.DC$CC*log(mu.current) + (1-data.DC$CC)*log(1-mu.current))
     log_likelihood = outcome.T * np.log(mu) + (1 - outcome).T * np.log(1 - mu)
     deviance = -2 * log_likelihood
     np.savetxt('C:/project/data/AC/info_matrix' + str(site_number) + ".csv", info_matrix)
     np.savetxt('C:/project/data/AC/score_vector' + str(site_number) + ".csv", score_vect)
     np.savetxt('C:/project/data/AC/deviance' + str(site_number) + ".csv", deviance)
     np.savetxt('C:/project/data/AC/samples' + str(site_number) + ".csv", [rows_number])
-    return info_matrix, score_vect, deviance, rows_number
-
+    return{
+        "info_matrix": info_matrix,
+        "score_vector": score_vect,
+        "deviance": deviance,
+        "rows_number": rows_number
+    }
 
 data_file = "C:/project/data/Study.1.csv"
 coefficients_file = "C:/project/data/AC/beta.vect.next.csv"
--- server_code.py
+++ server_code.py
@@ -1,26 +1,90 @@
 import numpy as np
+import sys
+import os
+import json
+import time
+import math
 
+from pytaskmanager.node.FlaskIO import ClientContainerProtocol
 
-def calclulate_beta(matrices, scores, deviances, samples):
-    info_matrix = np.empty([4, 4])
-    for matrix in matrices:
-        info_matrix += matrix
-    score_vector = np.empty(4)
-    for score in scores:
-        score_vector += score
+# loggers
+info = lambda msg: sys.stdout.write("info > "+msg+"\n")
+
+def calculate_coefficients(token, matrices, scores, deviances, samples):
+
+    info("Setup server communication client")
+    client = ClientContainerProtocol(
+        token=token,
+        host=os.environ["HOST"],
+        port=os.environ["PORT"],
+        path=os.environ["API_PATH"]
+    )
+
+    iterations_number = 0
+    epsilon = math.pow(10, -8)
+    max_iterations = math.pow(10, 10)
+    deviance_previous_iteration = math.pow(10, 10)
     deviance = 0
-    for site_deviance in deviances:
-        deviance += site_deviance
-    rows = 0
-    for sample in samples:
-         rows += sample
+    beta = [0, 0, 0, 0]
 
-    variance_covariance_matrix = np.linalg.inv(info_matrix)
-    new_beta =  variance_covariance_matrix * np.asmatrix(score_vector).T
-    print(score_vector)
-    print(variance_covariance_matrix)
-    print(new_beta)
-    return 0
+    while iterations_number == 0 or \
+            (abs(deviance - deviance_previous_iteration) > epsilon and
+             iterations_number < max_iterations):
+
+        deviance_previous_iteration = deviance
+        iterations_number += 1
+
+        # define the input for the summary algorithm
+        info("Defining input paramaeters")
+        input_ = {
+            "method": "summary",
+            "args": [],
+            "kwargs": {
+                "coefficients": beta
+            }
+        }
+
+        # collaboration and image is stored in the key, so we do not need
+        # to specify these
+        info("Creating node tasks")
+        task = client.create_new_task(input_)
+
+        # wait for all results
+        # TODO subscribe to websocket, to avoid polling
+        task_id = task.get("id")
+        task = client.request(f"task/{task_id}")
+        while not task.get("complete"):
+            task = client.request(f"task/{task_id}")
+            info("Waiting for results")
+            time.sleep(1)
+
+        info("Obtaining results")
+        results = client.get_results(task_id=task.get("id"))
+        results = [json.loads(result.get("result")) for result in results]
+
+
+        #TODO check how summation works on matrices
+        info_matrix = sum([node_result["info_matrix"] for node_result in results])
+        score_vector = sum([node_result["score_Vector"] for node_result in results])
+        deviance = sum([node_result["deviance"] for node_result in results])
+        rows = sum([node_result["rows"] for node_result in results])
+
+        variance_covariance_matrix = np.linalg.inv(info_matrix)
+        beta = variance_covariance_matrix * np.asmatrix(score_vector).T
+        info("SUMMARY OF MODEL AFTER ITERATION " + iterations_number)
+        info("deviance =" + deviance + "on " +
+            (rows - len(beta)) + " degrees of freedom")
+        info("deviance after previous iteration = "
+             + deviance_previous_iteration)
+        info("Variance covariance matrix is equal to ")
+        info(info_matrix)
+        info("Score vector is equal to")
+        info(score_vector)
+        info("beta is " + beta + "\n")
+
+    info("Final beta =" + beta)
+    info("Total number of iterations is" + iterations_number)
+    return beta
 
 
 matrices = []
