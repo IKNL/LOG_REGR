5ad1d65239235d12f5d302f4d16e71666f34aae5
Maksim Hrytsenia <m.hrytsenia@iknl.nl>
2019-04-23 12:04:18 +0200

    modify algorithm to allow log. regr of any datafile

--- local_main.py
+++ local_main.py
@@ -1,3 +1,8 @@
 import server_code
+import numpy as np
+import json
 
-server_code.calculate_coefficients()
+with open('local_config.json') as config:
+    data = json.load(config)
+
+server_code.calculate_coefficients(beta=np.asmatrix(np.zeros(data["nodes"]["number_of_parameters"])))
--- node_code.py
+++ node_code.py
@@ -5,61 +5,58 @@ import sys
 info = lambda msg: sys.stdout.write("info > " + msg + "\n")
 warn = lambda msg: sys.stdout.write("warn > " + msg + "\n")
 
-def make_local_iteration(coefficients, data_file=""):
 
-    separator = ","
-    #read database file
-    if not data_file:
-        node_data = pd.read_csv(os.environ["DATABASE_URI"], sep=separator)
-    else:
-        node_data = pd.read_csv(data_file, sep=separator)
+def make_local_iteration(coefficients, columns, outcome_variable, separator, local=True, data_file=""):
+    if not local:
+        data_file = os.environ["DATABASE_URI"]
+    design_matrix = pd.read_csv(data_file, sep=separator)[columns + [outcome_variable]]
 
-
+    outcome = np.matrix(design_matrix[outcome_variable]).T
+    info("Outcome shape is: " + str(outcome.shape))
+    #Remove the predictor from the design matrix
+    design_matrix = design_matrix.drop([outcome_variable], axis=1)
     #Calculate number of subjects available in the current study
     #(by enumerating length of ID column)
-    rows_number = len(node_data)
-
-    #Define design matrix (matrix of covariates) to contain BMI, SNP and the
-    #interaction covariate and add a column of 1s at the start for the regression constant
-    design_matrix = pd.concat([pd.Series(np.repeat([1], [rows_number], axis = 0)),
-                              node_data["bmi"], node_data["bmi456"],
-                               node_data["snp"]], axis=1).values
-
+    rows_number = len(design_matrix)
 
     # Calculate linear predictors from observed covariate values
     # and elements of
     # current beta vector
-    logit = np.dot(design_matrix, coefficients)
-
+    coefficients = np.asmatrix(coefficients)
+    info("Coefficients shape is: " + str(coefficients.shape))
+    logit = np.dot(design_matrix, coefficients.T)
+    info("Logit shape is: " + str(logit.shape))
     # Apply inverse logistic transformation
     odds_ratio = np.exp(logit)
+    info("Odds ration shape is: " + str(odds_ratio.shape))
     probability = odds_ratio / (1 + odds_ratio)
+    info("Probability shape is: " + str(probability.shape))
 
     # Derive variance function and diagonal elements for
     # weight matrix (using squared
     # first differential of link function)
     variance = np.multiply(probability, (1 - probability))
+    info("Variance shape is: " + str(variance.shape))
     link_function_differential = 1 / (variance)
     weight_matrix = np.diagflat(variance)
 
     #Calculate information matrix
     info_matrix = np.dot(np.dot(design_matrix.transpose(),
                                 weight_matrix), design_matrix)
-    info("Info matrix is: ")
-    info(str(info_matrix))
+
+    info("Info matrix shape is: "+ str(info_matrix.shape))
     #Derive u terms for score vector
-    outcome = np.matrix(node_data["CC"].values)
-    u_terms = np.multiply((outcome.T - probability), link_function_differential)
+    u_terms = np.multiply((outcome - probability), link_function_differential)
     #Calculate score vector
+    info("U terms shape is : " + str(u_terms.shape))
     score_vect = np.dot(np.dot(design_matrix.T, weight_matrix), u_terms)
-    info("Score vector is: ")
-    info(str(score_vect))
+    info("Score vector shape is: " + str(score_vect.shape))
     #Calculate log likelihood and deviance contribution for current study
     #For convenience, ignore the element of deviance that relates to the full saturated
     # model, because that will cancel out in calculating the change in deviance from one
     # iteration to the next (Dev.total – Dev.old [see below]) because the element relating
     # to the saturated model will be the same at every iteration).
-    log_likelihood = (np.dot(outcome, np.log(probability)) + np.dot((1 - outcome), np.log(1 - probability))).item(0)
+    log_likelihood = (np.dot(outcome.T, np.log(probability)) + np.dot((1 - outcome).T, np.log(1 - probability))).item(0)
     warn(str(log_likelihood))
     deviance = -2 * log_likelihood
     #ToDO add dimensions of array if it is needed for array reconstruction
