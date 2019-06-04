85eb81d3df8527a941c4653fe8350d005786d204
Maksim <maxim3631@gmail.com>
2019-06-04 10:59:09 +0200

    extract data part from node code in NR method

--- Newton-Raphson/node_code.py
+++ Newton-Raphson/node_code.py
@@ -6,17 +6,13 @@ info = lambda msg: sys.stdout.write("info > " + msg + "\n")
 warn = lambda msg: sys.stdout.write("warn > " + msg + "\n")
 
 
-def make_local_iteration(coefficients, columns, outcome_variable, separator, local=True, data_file=""):
-    if not local:
-        data_file = os.environ["DATABASE_URI"]
-    design_matrix = pd.read_csv(data_file, sep=separator)[columns + [outcome_variable]]
-
+def iterate(coefficients, outcome_variable, design_matrix):
     outcome = np.matrix(design_matrix[outcome_variable]).T
     info("Outcome shape is: " + str(outcome.shape))
-    #Remove the predictor from the design matrix
+    # Remove the predictor from the design matrix
     design_matrix = design_matrix.drop([outcome_variable], axis=1)
-    #Calculate number of subjects available in the current study
-    #(by enumerating length of ID column)
+    # Calculate number of subjects available in the current study
+    # (by enumerating length of ID column)
     rows_number = len(design_matrix)
 
     # Calculate linear predictors from observed covariate values
@@ -40,30 +36,38 @@ def make_local_iteration(coefficients, columns, outcome_variable, separator, loc
     link_function_differential = 1 / (variance)
     weight_matrix = np.diagflat(variance)
 
-    #Calculate information matrix
+    # Calculate information matrix
     info_matrix = np.dot(np.dot(design_matrix.transpose(),
                                 weight_matrix), design_matrix)
 
-    info("Info matrix shape is: "+ str(info_matrix.shape))
-    #Derive u terms for score vector
+    info("Info matrix shape is: " + str(info_matrix.shape))
+    # Derive u terms for score vector
     u_terms = np.multiply((outcome - probability), link_function_differential)
-    #Calculate score vector
+    # Calculate score vector
     info("U terms shape is : " + str(u_terms.shape))
     score_vect = np.dot(np.dot(design_matrix.T, weight_matrix), u_terms)
     info("Score vector shape is: " + str(score_vect.shape))
-    #Calculate log likelihood and deviance contribution for current study
-    #For convenience, ignore the element of deviance that relates to the full saturated
+    # Calculate log likelihood and deviance contribution for current study
+    # For convenience, ignore the element of deviance that relates to the full saturated
     # model, because that will cancel out in calculating the change in deviance from one
     # iteration to the next (Dev.total – Dev.old [see below]) because the element relating
     # to the saturated model will be the same at every iteration).
     log_likelihood = (np.dot(outcome.T, np.log(probability)) + np.dot((1 - outcome).T, np.log(1 - probability))).item(0)
     warn(str(log_likelihood))
     deviance = -2 * log_likelihood
-    #ToDO add dimensions of array if it is needed for array reconstruction
-    return{
+    # ToDO add dimensions of array if it is needed for array reconstruction
+    return {
         "info_matrix": info_matrix.tolist(),
         "score_vector": score_vect.tolist(),
         "deviance": deviance,
         "rows_number": rows_number
     }
+
+
+def make_local_iteration(coefficients, columns, outcome_variable, separator, local=True, data_file=""):
+    if not local:
+        data_file = os.environ["DATABASE_URI"]
+    design_matrix = pd.read_csv(data_file, sep=separator)[columns + [outcome_variable]]
+    return iterate(coefficients, outcome_variable, design_matrix)
+
 
