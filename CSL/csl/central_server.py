fix code of methods and add computation of running time

CSL/csl/central_server.py
@@ -21,8 +21,9 @@ class Central_Node(Node):
 
     # calculated using local data and using MLE function
     def get_optimized_coefficients(self):
-        results = minimize(self.calculate_log_likelihood, self.current_coefficients, method='L-BFGS-B')
-        return results["x"]
+        results = minimize(self.calculate_log_likelihood, self.current_coefficients,
+                           method='L-BFGS-B', tol=1e-6)
+        return results["x"] * 0
 
     def append_second_node(self, node):
         self.second_nodes.append(node)
@@ -30,8 +31,8 @@ class Central_Node(Node):
     def calculate_log_likelihood(self, coefficients):
         logit = self.get_logit(coefficients)
         # uses formula 2 for calculations
-        return (np.sum(np.log(1 + np.exp(logit))) - np.asscalar(np.dot(np.transpose(self.outcomes), logit))) / len(
-            self.outcomes)
+        return (np.sum(np.log(1 + np.exp(logit))) - np.asscalar(np.dot(np.transpose(self.outcomes), logit))) \
+               / len(self.outcomes)
 
     def calculate_node_gradient(self, node, coefficients):
         self.gradients_all_sites.append(node.calculate_log_likelihood_gradient(coefficients))
@@ -51,8 +52,8 @@ class Central_Node(Node):
         while len(self.gradients_all_sites) != len(self.second_nodes) + 1:
             time.sleep(0.1)
         gradients_sum = np.zeros((self.covariates.shape[1], 1))
-        for node_results in self.gradients_all_sites:
-            gradients_sum = np.add(gradients_sum, node_results)
+        for node_gradient in self.gradients_all_sites:
+            gradients_sum = np.add(gradients_sum, node_gradient)
         # uses part in brackets of formula (3) for calculations
         self.global_gradient = central_gradient - (gradients_sum / len(self.gradients_all_sites))
 
@@ -74,33 +75,37 @@ class Central_Node(Node):
         self.current_coefficients = self.get_optimized_coefficients()
         with open(log_file, "w") as file:
             file.write("Coefficients before iterations start are: {}\n".format(self.current_coefficients))
-
         # it calculates the gradient term which is inside the bracket in formula (3) Take into account that it required to
         # be calculated only once
         if not is_odal:
-            max_iterations = 100
+            max_iterations = 50
             max_delta = 1e-3
             converged = False
             final_number_of_iterations = max_iterations
 
+        running_time = 0
         with open(log_file, "a") as file:
             if is_odal:
+                start_time = time.time()
                 self.calculate_global_gradient()
                 # make an update as in formula (3), gradient is saved into class variable and used inside hte formula
                 # coefficients are passed as parameter because they would be optimized inside the code
                 self.current_coefficients = \
-                    minimize(self.calculate_surrogare_likelihood, self.current_coefficients, method='l-bfgs-b')["x"]
+                    minimize(self.calculate_surrogare_likelihood, self.current_coefficients,
+                             method='L-BFGS-B', tol=1e-6)["x"]
+                running_time = time.time() - start_time
             else:
+                start_time = time.time()
                 for iteration in range(0, max_iterations):
                     self.calculate_global_gradient()
                     # make an update as in formula (3), gradient is saved into class variable and used inside hte formula
                     # coefficients are passed as parameter because they would be optimized inside the code
                     previous_coefficients = self.current_coefficients
-                    self.current_coefficients = \
-                        minimize(self.calculate_surrogare_likelihood, self.current_coefficients, method='L-BFGS-B')["x"]
-                    file.write(
-                        "Coefficients after iteration {} are: {}\n".format(iteration + 1, self.current_coefficients))
+                    optimization_results = minimize(self.calculate_surrogare_likelihood,
+                                 self.current_coefficients, method='L-BFGS-B', tol=1e-6)
+                    self.current_coefficients = optimization_results["x"]
                     if self.get_vectors_difference(self.current_coefficients, previous_coefficients) < max_delta:
+                        running_time = time.time() - start_time
                         converged = True
                         final_number_of_iterations = iteration
                         break
@@ -110,6 +115,7 @@ class Central_Node(Node):
                 if not is_odal:
                     data["iterations"] = final_number_of_iterations
                     data["is_converged"] = converged
+                data["running_time"] = running_time
                 data["coefficients"] = {}
                 coefficient_index = 0
                 for covariate in self.covariates.columns:

CSL/csl/node.py
@@ -31,6 +31,7 @@ class Node:
         self.data = data
 
     def get_logit(self, coefficients):
+        # None is added to be able to transpose the vector
         return np.dot(self.covariates, coefficients[None].T)
 
     # do not init coefficients with 0
@@ -38,5 +39,6 @@ class Node:
     # test with manual data exists
     def calculate_log_likelihood_gradient(self, coefficients):
         logit = self.get_logit(coefficients)
-        logit_exp = np.exp(logit)
-        return np.dot(self.covariates.T, (logit_exp / (1 + logit_exp)) - self.outcomes) / len(self.outcomes)
+        logit_exp = np.exp(-logit)
+        return np.dot(self.covariates.T, (1 /
+                                          (1 + logit_exp)) - self.outcomes) / len(self.outcomes)

Newton-Raphson/newton_raphson/server_code.py
@@ -100,19 +100,21 @@ def make_iteration(beta, client):
 
     variance_covariance_matrix = np.linalg.inv(info_matrix)
     beta = (variance_covariance_matrix * np.asmatrix(score_vector)).T
+    start_writing_time = time.time()
     if not log_file:
         info("Variance covariance matrix is equal to\n {}\n\n".format(info_matrix))
         info("Score vector is equal to\n {}\n\n".format(score_vector))
         info("beta update is {}\n\n".format(beta))
         info("deviance = {}\n\n".format(deviance))
     else:
+
         with open(log_file, "w+") as file:
             file.write("Variance covariance matrix is equal to\n {}\n\n".format(info_matrix))
             file.write("Score vector is equal to\n {}\n\n".format(score_vector))
             file.write("beta update is {}\n\n".format(beta))
             file.write("deviance = {}\n\n".format(deviance))
-
-    return deviance, beta
+    not_calculating_time = time.time() - start_writing_time
+    return deviance, beta, not_calculating_time
 
 
 def get_client(token):
@@ -152,16 +154,20 @@ def calculate_coefficients(beta, result_file=None, token=""):
     #info("Setup server communication client")
     client = get_client(token)
     iterations_number = 0
-    epsilon = math.pow(10, -8)
+    epsilon = math.pow(10, -6)
     max_iterations = 100
     deviance_previous_iteration = math.pow(10, 10)
     deviance = -math.pow(10, 10)
+    start_time = time.time()
+    # some time is spend on writing into files, I extract this time from the final to achieve more reliable result
+    not_calculations_time = 0
 
     while iterations_number == 0 or \
             (abs(deviance - deviance_previous_iteration) > epsilon and
              iterations_number < max_iterations):
         iterations_number += 1
         deviance_previous_iteration = deviance
+        writing_start_time = time.time()
         if log_file is None:
             info("SUMMARY OF MODEL AFTER ITERATION {}".format(iterations_number))
             info("deviance after previous iteration = {}".format(deviance_previous_iteration))
@@ -170,14 +176,18 @@ def calculate_coefficients(beta, result_file=None, token=""):
                 file.write("SUMMARY OF MODEL AFTER ITERATION {}".format(iterations_number))
                 file.write("deviance after previous iteration = {}"
                            .format(deviance_previous_iteration))
-        deviance, beta_update = make_iteration(beta, client)
+        not_calculations_time += time.time() - writing_start_time
+        deviance, beta_update, iteration_not_calculation_time = make_iteration(beta, client)
+        not_calculations_time += iteration_not_calculation_time
         beta += beta_update
+    running_time = time.time() - start_time - not_calculations_time
     beta = np.array(beta.T)
     if result_file is not None:
         with open(result_file, "w+") as file:
             data = {}
             data["iterations"] = iterations_number
             data["is_converged"] = True if (iterations_number != max_iterations) else False
+            data["running_time"] = running_time
             data["coefficients"] = {}
             coefficient_index = 0
             for column_index in range(len(columns)):